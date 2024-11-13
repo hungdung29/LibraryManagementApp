82b27540567823498557327c2bed2b788f48db71
phuocsonuet <162415992+phuocsonuet@users.noreply.github.com>
2024-11-13 23:18:57 +0700

    update user info controller

--- src/main/java/org/app/Controller/UserInfoController.java
+++ src/main/java/org/app/Controller/UserInfoController.java
@@ -3,6 +3,9 @@ package org.app.Controller;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import org.app.MainApp;
+
+import java.io.IOException;
 
 public class UserInfoController {
     public Label nameLabel;
@@ -14,8 +17,18 @@ public class UserInfoController {
     public Button logOutButton;
 
     public void onChangePassButtonClicked(ActionEvent actionEvent) {
+        try {
+            MainApp.navigateToScene("change-password-view.fxml");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void onLogOutButtonClicked(ActionEvent actionEvent) {
+        try {
+            MainApp.navigateToScene("hello-view.fxml");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
