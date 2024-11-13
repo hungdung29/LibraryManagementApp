9df48ee5921bd8c485be6a82d27527e26b8572ea
Phan Tho <phantho.vt.vp@gmail.com>
2024-11-13 18:14:52 +0700

    bug show different table

--- src/main/java/org/app/Controller/BookController.java
+++ src/main/java/org/app/Controller/BookController.java
@@ -1,15 +1,25 @@
 package org.app.Controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+
+import javafx.scene.control.cell.PropertyValueFactory;
 import org.app.Object.Book;
 
-abstract public class BookController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class BookController implements Initializable {
     public TextField searchTextField;
     public Button searchButton;
+
     public TableView<Book> bookTable;
 
     public TableColumn<Book, String> titleColumn;
@@ -17,6 +27,33 @@ abstract public class BookController {
     public TableColumn<Book, String> isbnBorrowColumn;
     public TableColumn<Book, Integer> remainingColumn;
 
+    private final ObservableList<Book> books = FXCollections.observableArrayList();
+
     public void onSearchButtonClicked(ActionEvent actionEvent) {
+
+    }
+
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        bookTable.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
+        setUpColumns();
+
+        showBooks(UserController.tabType, UserController.username);
+    }
+
+    private void setUpColumns() {
+        titleColumn.setCellValueFactory(new PropertyValueFactory<Book, String>("title"));
+        authorColumn.setCellValueFactory(new PropertyValueFactory<Book, String>("author"));
+        isbnBorrowColumn.setCellValueFactory(new PropertyValueFactory<Book, String>("isbn"));
+        remainingColumn.setCellValueFactory(new PropertyValueFactory<Book, Integer>("remaining"));
+    }
+
+    public void showBooks(String tabType, String username) {
+        books.clear();
+        if (tabType.equals("borrow")) {
+            books.add(new Book("muon", "muon", "muon", "muon", 5));
+        } else if (tabType.equals("return")) {
+            books.add(new Book("tra", "tra", "tra", "tra", 5));
+        }
+        bookTable.setItems(books);
     }
 }
--- src/main/java/org/app/Controller/UserController.java
+++ src/main/java/org/app/Controller/UserController.java
@@ -1,9 +1,37 @@
 package org.app.Controller;
 
+import javafx.fxml.Initializable;
 import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
 
-public class UserController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class UserController implements Initializable {
+    public static String username = "default";
+    public static String tabType = "borrow";
+
+    public TabPane userTabPane;
+
     public Tab user_info;
     public Tab borrow_book;
     public Tab return_book;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        userTabPane.getSelectionModel().selectedItemProperty().addListener((obs, oldTab, newTab) -> {
+            if (newTab == borrow_book) {
+                UserController.tabType = "borrow";
+            } else if (newTab == return_book) {
+                UserController.tabType = "return";
+            }
+        });
+    }
+
+    public void setUsername(String username) {
+        UserController.username = username;
+    }
+
+
 }
