703f2e5fb4df12b5409abd24c96738a8e0c81cfa
hungdung29 <dung292005@gmail.com>
2024-11-14 01:15:23 +0700

    Update Book.java

--- src/main/java/org/app/Object/Book.java
+++ src/main/java/org/app/Object/Book.java
@@ -1,34 +1,27 @@
 package org.app.Object;
 
-public class Book {
-    private String title;
-    private String author;
+public class Book extends Document {
     private String publisher;
-    private String isbn;
-    private int remaining;
 
-    public Book(String title, String author, String publisher, String isbn, int remaining) {
-        this.title = title;
-        this.author = author;
+    public Book(String title, String author, String documentID, String publisher) {
+        super(title, author, documentID);
         this.publisher = publisher;
-        this.isbn = isbn;
-        this.remaining = remaining;
     }
 
     public String getTitle() {
-        return title;
+        return super.getTitle();
     }
 
     public void setTitle(String title) {
-        this.title = title;
+        super.setTitle(title);
     }
 
     public String getAuthor() {
-        return author;
+        return super.getAuthor();
     }
 
-    public void setAuthor(String author) {
-        this.author = author;
+    public void setBookAuthor(String author) {
+        super.setAuthor(author);
     }
 
     public String getPublisher() {
@@ -39,12 +32,12 @@ public class Book {
         this.publisher = publisher;
     }
 
-    public String getIsbn() {
-        return isbn;
+    public String getBookISBN() {
+        return super.getDocumentID();
     }
 
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
+    public void setBookISBN(String isbn) {
+        super.setDocumentID(isbn);
     }
 
     public int getRemaining() {
