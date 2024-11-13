17d9b0b8e1a2c21f5f547d68f4914b4a3adba1d8
hungdung29 <dung292005@gmail.com>
2024-11-14 01:15:26 +0700

    Update BookController.java

--- src/main/java/org/app/Controller/BookController.java
+++ src/main/java/org/app/Controller/BookController.java
@@ -1,16 +1,26 @@
 package org.app.Controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 import org.app.Object.Book;
+
+import java.util.function.Predicate;
 
 abstract public class BookController {
     public TextField searchTextField;
     public Button searchButton;
-    public TableView<Book> bookTable;
+    public TableView<Book> bookTableView;
+
+    ObservableList<Book> data = FXCollections.observableArrayList();
+    ObservableList<Book> books = FXCollections.observableArrayList();
 
     public TableColumn<Book, String> titleColumn;
     public TableColumn<Book, String> authorColumn;
@@ -18,5 +28,33 @@ abstract public class BookController {
     public TableColumn<Book, Integer> remainingColumn;
 
     public void onSearchButtonClicked(ActionEvent actionEvent) {
+        FilteredList<Book> filteredList = new FilteredList<>(data, p -> true);
+        searchTextField.textProperty().addListener((ObservableValue, oldValue, newValue) -> {
+            filteredList.setPredicate((Predicate<? super Book>) book -> {
+                if (newValue == null || newValue.isEmpty()) {
+                    return true;
+                }
+                String lowerCaseFilter = newValue.toLowerCase();
+                if (book.getBookISBN().toLowerCase().contains(lowerCaseFilter)) {
+                    return true;
+                }
+                if (book.getTitle().toLowerCase().contains(lowerCaseFilter)) {
+                    return true;
+                }
+                if (book.getAuthor().toLowerCase().contains(lowerCaseFilter)) {
+                    return true;
+                }
+                if (book.getPublisher().toLowerCase().contains(lowerCaseFilter)) {
+                    return true;
+                }
+                bookTableView.setPlaceholder(new Text("No record match your search"));
+                return false;
+            });
+            SortedList<Book> sortedList = new SortedList<>(filteredList);
+            sortedList.comparatorProperty().bind(bookTableView.comparatorProperty());
+            bookTableView.getItems().setAll(sortedList);
+        });
     }
+
+
 }
